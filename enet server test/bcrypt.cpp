b8b4a7061164bc3c2eaff6fb0379600f23aa074a
Taha Abdulgader (LetsChill) <74666116+LetsChill@users.noreply.github.com>
2021-06-30 07:18:24 +0200

    Update bcrypt.cpp

--- enet server test/bcrypt.cpp
+++ enet server test/bcrypt.cpp
@@ -25,7 +25,7 @@
 */
 
 #include <stdlib.h>
-#include <sys/io.h>
+#include <io.h>
 //#include <getopt.h> /* getopt at: https://gist.github.com/ashelly/7776712 */
 #include <process.h> /* for getpid() and the exec..() family */
 #include <direct.h> /* for _getcwd() and _chdir() */
